cd304e470dfd4fcf97a4be70b6a57471134e44aa
david <kadalilmessi@outlook.com>
2025-08-02 19:47:41 -0400

    Make DynamoDB configuration reusable and clean - TFLint and fmt fixes

--- envs/dev/s3/backend.tf
+++ envs/dev/s3/backend.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     # These will be overridden by -backend-config in CI/CD
-    bucket         = "kada-terraform-eks-state-s3-bucket" 
+    bucket         = "kada-terraform-eks-state-s3-bucket"
     key            = "s3-new-oidc-dev-terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "kada-terraform-eks-state-lock"
